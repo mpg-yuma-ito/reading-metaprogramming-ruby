26864caf4ba2a72840291d7bf1e2a21aff83537c
mpg-yuma-ito <97879444+mpg-yuma-ito@users.noreply.github.com>
2023-02-12 15:16:00 +0900

    02_define.rb 解答（一部TODO）

--- 03_method/02_define.rb
+++ 03_method/02_define.rb
@@ -1,6 +1,11 @@
 # Q1.
 # 次の動作をする A1 class を実装する
 # - "//" を返す "//"メソッドが存在すること
+class A1
+  define_method '//' do
+    '//'
+  end
+end
 
 # Q2.
 # 次の動作をする A2 class を実装する
@@ -11,8 +16,45 @@
 #   - 引数がnilの場合は、dev_teamメソッドを呼ぶこと
 # - また、2で定義するメソッドは以下を満たすものとする
 #   - メソッドが定義されるのは同時に生成されるオブジェクトのみで、別のA2インスタンスには（同じ値を含む配列を生成時に渡さない限り）定義されない
+class A2
+  def initialize(array)
+    # オブジェクトの特異クラスにメソッドを定義する
+    array.each do |val|
+      define_singleton_method "hoge_#{val}" do |num|
+        return dev_team if num.nil?
+
+        "hoge_#{val}" * num
+      end
+    end
+  end
+
+  def dev_team
+    "SmartHR Dev Team"
+  end
+end
 
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+module OriginalAccessor
+  def self.included(klass)
+
+    def klass.my_attr_accessor(name)
+      define_method name do
+        @original_accessor_hash&.[](name)
+      end
+
+      define_method "#{name}=" do |val|
+        @original_accessor_hash ||= {}
+        @original_accessor_hash[name] = val
+
+        if val.is_a?(TrueClass) || val.is_a?(FalseClass)
+          def self.hoge? # TODO 修正
+            @original_accessor_hash[:hoge]
+          end
+        end
+      end
+    end
+  end
+end
